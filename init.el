2edd58f14e4adb57e595ac7bc975c23fbe3c44ab
Curtis Ault <curtisault@gmail.com>
2018-07-01 22:15:51 -0500

    Update init.el

--- init.el
+++ init.el
@@ -22,8 +22,14 @@
 ;; Vim mode
 (use-package evil
   :ensure t
+  :config
+  (evil-mode 1))
+
+;; evil-escape
+(use-package evil-escape
+  :ensure t
   :init
   (setq-default evil-escape-key-sequence "jk")
   :config
-  (evil-mode 1))
+  (evil-escape-mode 1))
 
